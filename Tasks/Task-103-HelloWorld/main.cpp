c2c258763bfd79aa8134efa3e7ad4635a6e8fedf
Pat Colyn <patcolyn@gmail.com>
2023-10-06 10:46:12 +0100

    Task 103 - Hello World Complete

--- Tasks/Task-103-HelloWorld/main.cpp
+++ Tasks/Task-103-HelloWorld/main.cpp
@@ -33,12 +33,43 @@ int main()
 
     // This is a variable (not an oject as it has no functions) that stores a whole number (integer) in memory
     // (I used the keyword volatile to force it to use memory... long story and one for later)
-    volatile int counter = 0;
+    
+    // Task 12
+    //volatile int counter = 0;
 
-    while (true)
+    volatile int counter = 10;
+
+    // Task 11
+    //while (counter < 5)
+
+
+    while (counter < 16)
     {
         //Toggle the LED
         ledBlue = !ledBlue;
+
+        //Display in the terminal
+        printf("Count: %d\n", counter);
+
+        //Wait
+        wait_us(WAIT_TIME_MS * 1000);
+
+        //Add 1 to the counter "variable"
+        counter = counter + 1;        
+    }
+
+    ledRed = 1;
+    ledGreen = 1;
+    ledBlue = 1;
+
+    wait_us(osWaitForever);
+
+    /*while (true)
+    {
+        //Toggle the LED
+        ledBlue = !ledBlue;
+        ledRed = !ledRed;
+        ledGreen = !ledGreen;
 
         //Add 1 to the counter "variable"
         counter = counter + 1;
@@ -49,4 +80,5 @@ int main()
         //Wait
         wait_us(WAIT_TIME_MS * 1000);
     }
+    */
 }
