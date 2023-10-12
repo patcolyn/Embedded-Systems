403a590bf52cad2621c976b472e7a80c26464e26
Pat Colyn <patcolyn@gmail.com>
2023-10-12 11:57:45 +0100

    Task 112 - Terminal Input Complete

--- Tasks/Task-112-Terminalnput/main.cpp
+++ Tasks/Task-112-Terminalnput/main.cpp
@@ -23,19 +23,23 @@ int main()
     printf("You entered character %c which has the ASCII code %d\n", c2, c2);
 
     //scanf
-    printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");
-    int delay_ms;
-    int parsed = scanf("%d", &delay_ms);
+    printf("\n\nTo set the on delay (in ms), type in an integer number and press return\n");
+    int delay_on_ms;
+    int parsed = scanf("%d", &delay_on_ms);
+
+    printf("\n\nTo set the off delay (in ms), type in an integer number and press return\n");
+    int delay_off_ms;
+    parsed = scanf("%d", &delay_off_ms);
     
-    printf("You entered %d correct integer values. The value was %dms\n", parsed, delay_ms);
+    printf("You entered %d correct integer values. The values were %dms and %dms\n", parsed*2, delay_on_ms, delay_off_ms);
 
     // Stop
     while (parsed > 0) {
-        wait_us(delay_ms * 1000);
+        wait_us(delay_off_ms * 1000);
         led1 = 1;
         led2 = 1;
         led3 = 1;
-        wait_us(delay_ms * 1000);
+        wait_us(delay_on_ms * 1000);
         led1 = 0;
         led2 = 0;
         led3 = 0;        
